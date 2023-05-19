4fc294c2632970fe92d3237f0c185e3fe06e98c3
akibuis <akibu.ismail@gmail.com>
2023-05-19 22:23:10 +0100

    Get Data from API

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,41 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
 
-  constructor() { }
+export class HomepageComponent implements OnInit {
+  _SITETITLE:any;
+  _DOGPHOTO:any;
+  _FACEBOOK:any;
+  _WHATSAPP:any;
+  _ITEMS:any = [];
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    localStorage.setItem('Title', 'Angular Lesson');
+    localStorage.setItem('DogImage', 'assets/white-cat.jpeg');
+    localStorage.setItem('Facebook', 'https://facebook.com/akibuis');
+    localStorage.setItem('WhatsApp', 'https://whatsapp.com');
+    this.GetAllDatas();
+    this.GetNamesData();
+  }
+
+  GetAllDatas(){
+    this._SITETITLE = localStorage.getItem('Title');
+    this._DOGPHOTO = localStorage.getItem('DogImage');
+    this._FACEBOOK = localStorage.getItem('Facebook');
+    this._WHATSAPP = localStorage.getItem('WhatsApp');
+  }
+
+  GetNamesData(){
+    this.http.get("https://reqres.in/api/users?page=1").subscribe(
+      (res:any) => {
+        this._ITEMS = res.data;
+      },
+      (err:any) => {
+        console.log(err);
+      }
+    )
   }
 
 }
